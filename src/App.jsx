6eb357fad75f60e253ae436d8253e4aca9dd7ced
guilherme <guilherme_lfar@hotmail.com>
2026-02-24 18:46:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react"
+import Header from "./components/Header"
+
+function App() {
+  return (
+      <div className="bg-bg-primary min-h-screen">
+        <div>
+          <Header />
+        </div>
+      </div>
+  )
+}
+
+export default App
