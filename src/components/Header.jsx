6b46592a76e01b2e202cecae6308b321d3bebf04
guilherme <guilherme_lfar@hotmail.com>
2026-02-25 20:01:03 -0300

    terminando o header e iniciando o corpo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 function Header() {
   return (
-    <header className="flex justify-between border-b-2 border-slate-800/65 z-10 sticky top-0 bg-bg-primary text-white p-4">
-      <div className="flex justify-center items-center ">
+    <header className="flex justify-center border-b-2 border-slate-800/65 z-10 sticky top-0 bg-bg-primary text-white p-4">
+      <div className="flex justify-between items-center w-7xl">
+        <div className="flex justify-center items-center ">
         <h1 className="font-primary text-3xl ">
           Crypto <span className="text-text-destacado">Live</span>
         </h1>
       </div>
       <div>
         <a href="dashboard"><h6>Dashboard</h6></a>
+      </div>
       </div>
     </header>
   );
