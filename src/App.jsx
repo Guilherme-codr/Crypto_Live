18631a041688e9709e9bab59ac3106b0a0dbae44
guilherme <guilherme_lfar@hotmail.com>
2026-02-25 20:24:12 -0300

    terminando a hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react"
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 function App() {
   return (
       <div className="bg-bg-primary min-h-screen">
         <div>
           <Header />
+          <Hero />
         </div>
       </div>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section className="flex flex-col justify-center items-center gap-4 text-center py-20 font-secondary text-text-secondary">
+      <h2 className="font-primary text-3xl md:text-5xl pb-4 md:pb-9">
+        Domine o mercado de Moedas Digitais em <br></br><span className="text-text-destacado">Tempo Real</span>
+      </h2>
+      <p className="text-sm md:text-xl max-w-2xl">
+        Dados atualizados e análises profundas para você não perder nenhuma <span className="text-text-destacado">oportunidade</span>.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
